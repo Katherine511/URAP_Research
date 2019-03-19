da821eea4c1fb6cd142947c5c6c0a4b8ede3042f
KatherineZhang <xinyue511@outlook.com>
2019-03-18 23:43:30 -0700

    Add multitasking threads

--- addUnderscore.py
+++ addUnderscore.py
@@ -1,21 +1,26 @@
 import re
+from pathos import multiprocessing
 from flashtext.keyword import KeywordProcessor 
 
 title = open("nGramsCompNames.txt", "r")
-sentence = open("sentences_short.txt", "r")
+sentences = open("sentences_short.txt", "r")
 output = open("clean_sentences.txt", "w")
 
-keyword_processor = KeywordProcessor()
+keyword_processor = KeywordProcessor(case_sensitive = True)
+sentence = sentences.readlines() # create an iterable object of sentenes
+
 for line in title.readlines():
 	string = line.split()
-	# add target words to the keyword processor
 	keyword_processor.add_keyword(string[0].replace("_", " "), string[0]) 
 
-for sentence in sentence: 
+def addUnderscore(sentence):
 	target = keyword_processor.extract_keywords(sentence)
-	# replace words in the sentence with target words from nGramsCompNames.txt
 	new_line = keyword_processor.replace_keywords(sentence)
-	output.write(new_line)
+	return new_line
+
+pool = multiprocessing.Pool(processes = 4) 		# number of threads
+for result in pool.imap_unordered(addUnderscore, sentence):
+	output.write(result)
 
 output.close()
 
